25c15a90b17e09d3e71ee5281b381f81be5656ea
Salino3 <flaviobcn012@gmail.com>
2025-01-03 11:35:13 +0100

    exercise with Signal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Counter } from "./components/counter";
 
 function App() {
   return (
     <div className="rootApp">
-      <h1>Hello, World!</h1>
+      <Counter />
     </div>
   );
 }
---
+++ src/components/counter.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { useSignal } from "@preact/signals-react";
+
+export const Counter: React.FC = () => {
+  const count = useSignal(0);
+  const [counting, setCounting] = useState(0);
+
+  const increment = () => {
+    count.value += 1;
+    console.log("Signal Count:", count.value);
+  };
+
+  const increment2 = () => {
+    setCounting((prev) => (prev += 1));
+    console.log("useState Count: ", counting);
+  };
+
+  return (
+    <div>
+      <h1>Counter Example</h1>
+      <h4>check console.log</h4>
+      <p>Signal Count: {count}</p>
+      <button onClick={increment}>Increment with Signal</button>
+      <hr />
+      <p>useState Count: {counting}</p>
+      <button onClick={increment2}>Increment with useState</button>
+    </div>
+  );
+};
